fade2104cc8dd885b02b9f005f9eaa977e09d052
한도희 <sknskin@naver.com>
2026-02-04 21:55:01 +0900

    쿼리 로그 출력 - 주석 처리

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -3,6 +3,18 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+
+  constructor() {
+    super({
+      log: [
+        // 쿼리 출력을 원하는 경우 아래 코드 주석 해제
+        // { emit: 'stdout', level: 'query' },  // 실행되는 모든 SQL 쿼리 출력
+        { emit: 'stdout', level: 'info' },   // 일반적인 정보
+        { emit: 'stdout', level: 'warn' },   // 경고
+        { emit: 'stdout', level: 'error' },  // 에러
+      ],
+    })
+  }
   async onModuleInit() {
     await this.$connect();
   }
